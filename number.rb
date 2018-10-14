593b23ccd6c1c9d58520392ef4a08388d8452415
Eulandria Phillips <eulandria@mail.com>
2018-10-14 19:07:09 +0000

    automatically backed up by learn

---
+++ number.rb
@@ -0,0 +1,9 @@
+def my_method(array,number)
+  array.each do |array_number|
+    if array_number == number
+      
+    return TRUEend
+    return FALSEend
+    my_array = [1,2,3,10,50,124,513,4]
+    puts my_method (myrrr_array,51)
+    
