74885dd8b48146eac11dbe9ea260aab3e1d09656
Eulandria Phillips <eulandria@mail.com>
2018-10-14 18:56:20 +0000

    automatically backed up by learn

---
+++ movie.rb
@@ -0,0 +1,14 @@
+def my_method(array)
+  array.each do I movie
+  if movie == "Dumb and Dumber"
+  puts "My most favorit movie is#{movie}"
+  else
+     puts "One of my favorite movies is 
+     #{movie}
+
+  end
+end
+
+my_array = ["Dumb and Dumber","Remember the Titans"}
+
+mhy_method(my_array)
